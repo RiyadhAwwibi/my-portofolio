8de74c7864b6d7fbf08ca9987ff4c0e21126e750
RiyadhAwwibi <riyadhawwibi19@gmail.com>
2023-05-12 22:26:17 +0700

    'merubah asset dan text pada index html dan merubah quote js'

--- js/quote.js
+++ js/quote.js
@@ -1,24 +1,24 @@
 // Add single-quoted phrases or quotations here, separated
 // by commas, to be randomly shown on page load
 const quotes = [
-    // Ideal length ~= 30 characters OR 1 emoji + 27 characters
-    "🎶 Listening: Wonderwall",
-    "🎶 Listening: The Bidding",
-    "🎶 Listening: New Light",
-    "📺 Watching: Peaky Blinders",
-    "📺 Watching: Start-Up",
-    "📺 Watching: Breaking Bad",
-    "🎮 Playing: Chess",
-    "🎮 Playing: Valorant",
-    "🎮 Playing: Wordle",
-  ];
-  
-  function randomQuote() {
-    const quoteSpan = document.getElementById("quote");
-    if (quoteSpan) {
-      quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-    }
+  // Ideal length ~= 30 characters OR 1 emoji + 27 characters
+  '🎶 Listening: Basket Case',
+  '🎶 Listening: Human',
+  '🎶 Listening: New Light',
+  '📺 Watching: Peaky Blinders',
+  '📺 Watching: Prison Break',
+  '📺 Watching: Breaking Bad',
+  '🎮 Playing: Chess',
+  '🎮 Playing: Valorant',
+  '🎮 Playing: Wordle',
+];
+
+function randomQuote() {
+  const quoteSpan = document.getElementById('quote');
+  if (quoteSpan) {
+    quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
   }
-  
-  randomQuote();
-  window.setInterval(randomQuote, 5000);
+}
+
+randomQuote();
+window.setInterval(randomQuote, 5000);
